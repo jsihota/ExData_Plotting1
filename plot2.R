e15d26c456a38c41bed097703083df137eca440e
jsihota <jagdeep_s1@hotmail.com>
2014-07-11 19:08:43 -0700

    add plot2

---
+++ plot2.R
@@ -0,0 +1,26 @@
+## load data
+setwd("C:/Git/ExData_Plotting1")
+url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+filename <- 'exdata-data-household_power_consumption.zip'
+download.file(url, destfile = filename, method = 'auto')
+unzip(filename)
+
+
+
+## subset and format 
+data <- read.table("household_power_consumption.txt", header=T, sep=';', na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+head(data)
+data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"),]
+data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+head(data)
+summary(data)
+
+## Plot 
+##hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+
+plot(data$DateTime, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+
+## Saving to file
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
