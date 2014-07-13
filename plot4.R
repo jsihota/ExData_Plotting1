cd20d4250e6cc9a3a1367740af0db3aa0ec81586
jsihota <jagdeep_s1@hotmail.com>
2014-07-12 20:29:42 -0700

    finish 4th plot

--- plot4.R
+++ plot4.R
@@ -16,12 +16,23 @@ head(data)
 summary(data)
 
 ## Plot 
-cols = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-plot(data$DateTime,data$Sub_metering_1,xlab="", ylab="Enegey sub meeting",  
-     type="l")
-lines(data$DateTime,data$Sub_metering_2,type="l",col="red")
-lines(data$DateTime,data$Sub_metering_3,type="l",col="blue")
-legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend=cols)
+par (mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+with(data, {
+  plot(DateTime, Global_active_power, xlab="", ylab="Global Active Power", type="l")
+  plot(DateTime, Voltage, xlab="datetime", ylab="Voltage", type="l")
+  
+  cols = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+  plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  lines(DateTime, Sub_metering_2, type="l", col="red")
+  lines(DateTime, Sub_metering_3, type="l", col="blue")
+  legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend=cols, bty="n")
+  
+  plot(DateTime, Global_reactive_power, xlab="datetime", ylab="Global_reactive_power", type="l")        
+}) 
+
+## Saving to file
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
 
 
 ## Saving to file
